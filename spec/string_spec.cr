925f569b58f561c1d03626a08e76fd098d944a8b
Filip Prober <filip.prober@condition.at>
2025-09-16 10:54:43 +0200

    feat: added String.after() method

--- spec/string_spec.cr
+++ spec/string_spec.cr
@@ -2,6 +2,17 @@ require "spec"
 require "../src/string"
 
 describe String do
+  it "after" do
+    "hannah".after("han").should eq "nah"
+    "hannah".after("n").should eq "nah"
+    "ééé hannah".after("han").should eq "nah"
+    "hannah".after("xxxx").should eq "hannah"
+    "hannah".after("").should eq "hannah"
+    "han0nah".after("0").should eq "nah"
+    "han0nah".after(0).should eq "nah"
+    "han2nah".after(2).should eq "nah"
+  end
+
   it "reverse" do
     "FooBar".reverse.should eq "raBooF"
     "Teniszütő".reverse.should eq "őtüzsineT"
--- src/string.cr
+++ src/string.cr
@@ -1,5 +1,28 @@
 class String
   #
+  # Return the remainder of a string after the first occurrence of a given value.
+  #
+  # ```
+  # "Crystal Package".after("Crystal ") # => "Package"
+  # ```
+  def after(search : String) : String
+    index = self.index(search)
+
+    if index
+      return self[(index + search.size)..-1]
+    end
+
+    return self
+  end
+
+  #
+  # :ditto:
+  #
+  def after(search : Int32) : String
+    return self.after(search.to_s)
+  end
+
+  #
   # Reverse the given string.
   #
   # ```
